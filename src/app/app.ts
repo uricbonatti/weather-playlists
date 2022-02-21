26ae4765905d00d13f154e1ef12d69f59caded39
Uric Bonatti <uricbonatti.eng@gmail.com>
2022-02-20 23:16:54 -0300

    Remoção da rota /health

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,14 +6,12 @@ import corsConfig from '@config/corsConfig';
 import weatherSoundtrackRoutes from '@routes/weatherSoundtrackRoutes';
 import errorHandler from '@errors/errorHandler';
 import limiter from '@middlewares/limiter';
-import env from '@config/env';
 
 function app() {
   const expressApp = express();
 
   expressApp.use(cors(corsConfig));
   expressApp.use(express.json());
-  expressApp.get('/health', (req, res) => res.json(env));
   expressApp.use(limiter);
   expressApp.use(weatherSoundtrackRoutes);
 
